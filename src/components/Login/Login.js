cd89b6d12a46c5d7dfdd6400b086a8e9d6db9759
Ira Noschenko <i.noschenko@gmail.com>
2018-05-03 16:11:59 +0300

    login-> styled

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,12 +11,21 @@ class Login extends PureComponent {
 			<div className="login">
 				Получить токен нужно на своей странице github,
 				перейдите по{' '}
-				<a href="https://github.com/settings/tokens">
+				<a href="https://github.com/settings/tokens" target="_blank" rel="noopener noreferrer">
 					адресу
 				</a>{' '}
-				и создайте себе токен. Запишите куда-нибудь токен,
-				так как после создания доступ к нему будет только
-				один раз.
+				и создайте себе токен.
+				<p>Запишите куда-нибудь токен, так как после создания доступ к нему будет только один раз. </p>
+
+				<input className="form-field"
+					type="text"
+					placeholder="auth_token"
+
+					// value={ this.state.token }
+					// onChange={ this.handleOnChange }
+					// onKeyDown={ this.handleOnKeyDown }
+				/>
+				<span>После ввода нажмите Enter</span>
 			</div>
 		)
 	}
